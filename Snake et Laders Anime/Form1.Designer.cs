18f8ff0e9d819fb30a29eea7be95b8bb5dc9055b
NUNO.DJSSM <NUNO.DJSSM@dipgecfpt.onmicrosoft.com>
2022-05-09 13:36:47 +0200

    nuno

--- Snake et Laders Anime/Form1.Designer.cs
+++ Snake et Laders Anime/Form1.Designer.cs
@@ -31,6 +31,7 @@ namespace Snake_et_Laders_Anime
         {
             this.button1 = new System.Windows.Forms.Button();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -52,12 +53,21 @@ namespace Snake_et_Laders_Anime
             this.checkBox1.Text = "checkBox1";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(363, 214);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 31);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -71,6 +81,7 @@ namespace Snake_et_Laders_Anime
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
