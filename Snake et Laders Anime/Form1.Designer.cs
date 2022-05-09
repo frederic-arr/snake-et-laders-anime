c53f4069d574444f58cd690ebc1c0b73239eddf6
NUNO.DJSSM <NUNO.DJSSM@dipgecfpt.onmicrosoft.com>
2022-05-09 15:44:37 +0200

    nuno

--- Snake et Laders Anime/Form1.Designer.cs
+++ Snake et Laders Anime/Form1.Designer.cs
@@ -29,70 +29,20 @@ namespace Snake_et_Laders_Anime
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.buttonmescouilles = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(361, 135);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(504, 135);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(80, 17);
-            this.checkBox1.TabIndex = 1;
-            this.checkBox1.Text = "checkBox1";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            // 
-            // buttonmescouilles
-            // 
-            this.buttonmescouilles.Location = new System.Drawing.Point(518, 225);
-            this.buttonmescouilles.Name = "buttonmescouilles";
-            this.buttonmescouilles.Size = new System.Drawing.Size(210, 173);
-            this.buttonmescouilles.TabIndex = 2;
-            this.buttonmescouilles.Text = "button2";
-            this.buttonmescouilles.UseVisualStyleBackColor = true;
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(363, 214);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 31);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.buttonmescouilles);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Test";
+            this.Text = "Snake & Lader";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.Button buttonmescouilles;
-        private System.Windows.Forms.Button button2;
     }
 }
 
