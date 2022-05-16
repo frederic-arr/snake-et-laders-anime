3a60f3cde2a55fc257113015d324d40ba20f75ae
NUNO.DJSSM <NUNO.DJSSM@dipgecfpt.onmicrosoft.com>
2022-05-16 13:51:23 +0200

    Avancement Form Joueur

--- Snake et Laders Anime/Form1.Designer.cs
+++ Snake et Laders Anime/Form1.Designer.cs
@@ -29,32 +29,223 @@ namespace Snake_et_Laders_Anime
         /// </summary>
         private void InitializeComponent()
         {
-            this.CmBxNbJoueur = new System.Windows.Forms.ComboBox();
+            this.components = new System.ComponentModel.Container();
+            this.RBtn2Joueurs = new System.Windows.Forms.RadioButton();
+            this.RBtn3Joueurs = new System.Windows.Forms.RadioButton();
+            this.RBtn4Joueurs = new System.Windows.Forms.RadioButton();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.PBx9 = new System.Windows.Forms.PictureBox();
+            this.PBx8 = new System.Windows.Forms.PictureBox();
+            this.PBx7 = new System.Windows.Forms.PictureBox();
+            this.PBx6 = new System.Windows.Forms.PictureBox();
+            this.PBx5 = new System.Windows.Forms.PictureBox();
+            this.PBx4 = new System.Windows.Forms.PictureBox();
+            this.PBx3 = new System.Windows.Forms.PictureBox();
+            this.PBx2 = new System.Windows.Forms.PictureBox();
+            this.PBx1 = new System.Windows.Forms.PictureBox();
+            this.BtnJoueur = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PBx9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBx8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBx7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBx6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBx5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBx4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBx3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBx2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBx1)).BeginInit();
             this.SuspendLayout();
             // 
-            // CmBxNbJoueur
+            // RBtn2Joueurs
             // 
-            this.CmBxNbJoueur.FormattingEnabled = true;
-            this.CmBxNbJoueur.Location = new System.Drawing.Point(227, 96);
-            this.CmBxNbJoueur.Name = "CmBxNbJoueur";
-            this.CmBxNbJoueur.Size = new System.Drawing.Size(121, 21);
-            this.CmBxNbJoueur.TabIndex = 0;
+            this.RBtn2Joueurs.AutoSize = true;
+            this.RBtn2Joueurs.Location = new System.Drawing.Point(161, 97);
+            this.RBtn2Joueurs.Name = "RBtn2Joueurs";
+            this.RBtn2Joueurs.Size = new System.Drawing.Size(71, 17);
+            this.RBtn2Joueurs.TabIndex = 0;
+            this.RBtn2Joueurs.TabStop = true;
+            this.RBtn2Joueurs.Text = "2 Joueurs";
+            this.RBtn2Joueurs.UseVisualStyleBackColor = true;
+            // 
+            // RBtn3Joueurs
+            // 
+            this.RBtn3Joueurs.AutoSize = true;
+            this.RBtn3Joueurs.Location = new System.Drawing.Point(161, 184);
+            this.RBtn3Joueurs.Name = "RBtn3Joueurs";
+            this.RBtn3Joueurs.Size = new System.Drawing.Size(71, 17);
+            this.RBtn3Joueurs.TabIndex = 1;
+            this.RBtn3Joueurs.TabStop = true;
+            this.RBtn3Joueurs.Text = "3 Joueurs";
+            this.RBtn3Joueurs.UseVisualStyleBackColor = true;
+            // 
+            // RBtn4Joueurs
+            // 
+            this.RBtn4Joueurs.AutoSize = true;
+            this.RBtn4Joueurs.Location = new System.Drawing.Point(161, 280);
+            this.RBtn4Joueurs.Name = "RBtn4Joueurs";
+            this.RBtn4Joueurs.Size = new System.Drawing.Size(71, 17);
+            this.RBtn4Joueurs.TabIndex = 2;
+            this.RBtn4Joueurs.TabStop = true;
+            this.RBtn4Joueurs.Text = "4 Joueurs";
+            this.RBtn4Joueurs.UseVisualStyleBackColor = true;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // PBx9
+            // 
+            this.PBx9.Image = global::Snake_et_Laders_Anime.Properties.Resources.pionJaune_removebg_preview;
+            this.PBx9.Location = new System.Drawing.Point(465, 265);
+            this.PBx9.Name = "PBx9";
+            this.PBx9.Size = new System.Drawing.Size(50, 55);
+            this.PBx9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PBx9.TabIndex = 11;
+            this.PBx9.TabStop = false;
+            // 
+            // PBx8
+            // 
+            this.PBx8.Image = global::Snake_et_Laders_Anime.Properties.Resources.PionRouge_removebg_preview;
+            this.PBx8.Location = new System.Drawing.Point(384, 265);
+            this.PBx8.Name = "PBx8";
+            this.PBx8.Size = new System.Drawing.Size(50, 55);
+            this.PBx8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PBx8.TabIndex = 10;
+            this.PBx8.TabStop = false;
+            // 
+            // PBx7
+            // 
+            this.PBx7.Image = global::Snake_et_Laders_Anime.Properties.Resources.PionBleu_removebg_preview;
+            this.PBx7.Location = new System.Drawing.Point(309, 265);
+            this.PBx7.Name = "PBx7";
+            this.PBx7.Size = new System.Drawing.Size(50, 55);
+            this.PBx7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PBx7.TabIndex = 9;
+            this.PBx7.TabStop = false;
+            // 
+            // PBx6
+            // 
+            this.PBx6.Image = global::Snake_et_Laders_Anime.Properties.Resources.PionVert_removebg_preview;
+            this.PBx6.Location = new System.Drawing.Point(238, 265);
+            this.PBx6.Name = "PBx6";
+            this.PBx6.Size = new System.Drawing.Size(50, 55);
+            this.PBx6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PBx6.TabIndex = 8;
+            this.PBx6.TabStop = false;
+            // 
+            // PBx5
+            // 
+            this.PBx5.Image = global::Snake_et_Laders_Anime.Properties.Resources.PionRouge_removebg_preview;
+            this.PBx5.Location = new System.Drawing.Point(384, 165);
+            this.PBx5.Name = "PBx5";
+            this.PBx5.Size = new System.Drawing.Size(50, 55);
+            this.PBx5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PBx5.TabIndex = 7;
+            this.PBx5.TabStop = false;
+            // 
+            // PBx4
+            // 
+            this.PBx4.Image = global::Snake_et_Laders_Anime.Properties.Resources.PionBleu_removebg_preview;
+            this.PBx4.Location = new System.Drawing.Point(309, 165);
+            this.PBx4.Name = "PBx4";
+            this.PBx4.Size = new System.Drawing.Size(50, 55);
+            this.PBx4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PBx4.TabIndex = 6;
+            this.PBx4.TabStop = false;
+            this.PBx4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // PBx3
+            // 
+            this.PBx3.Image = global::Snake_et_Laders_Anime.Properties.Resources.PionVert_removebg_preview;
+            this.PBx3.Location = new System.Drawing.Point(238, 165);
+            this.PBx3.Name = "PBx3";
+            this.PBx3.Size = new System.Drawing.Size(50, 55);
+            this.PBx3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PBx3.TabIndex = 5;
+            this.PBx3.TabStop = false;
+            // 
+            // PBx2
+            // 
+            this.PBx2.Image = global::Snake_et_Laders_Anime.Properties.Resources.PionBleu_removebg_preview;
+            this.PBx2.Location = new System.Drawing.Point(309, 76);
+            this.PBx2.Name = "PBx2";
+            this.PBx2.Size = new System.Drawing.Size(50, 55);
+            this.PBx2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PBx2.TabIndex = 4;
+            this.PBx2.TabStop = false;
+            // 
+            // PBx1
+            // 
+            this.PBx1.Image = global::Snake_et_Laders_Anime.Properties.Resources.PionVert_removebg_preview;
+            this.PBx1.Location = new System.Drawing.Point(238, 76);
+            this.PBx1.Name = "PBx1";
+            this.PBx1.Size = new System.Drawing.Size(50, 55);
+            this.PBx1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PBx1.TabIndex = 3;
+            this.PBx1.TabStop = false;
+            // 
+            // BtnJoueur
+            // 
+            this.BtnJoueur.Location = new System.Drawing.Point(309, 362);
+            this.BtnJoueur.Name = "BtnJoueur";
+            this.BtnJoueur.Size = new System.Drawing.Size(113, 50);
+            this.BtnJoueur.TabIndex = 12;
+            this.BtnJoueur.Text = "Joueur";
+            this.BtnJoueur.UseVisualStyleBackColor = true;
+            this.BtnJoueur.Click += new System.EventHandler(this.BtnJoueur_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.CmBxNbJoueur);
+            this.Controls.Add(this.BtnJoueur);
+            this.Controls.Add(this.PBx9);
+            this.Controls.Add(this.PBx8);
+            this.Controls.Add(this.PBx7);
+            this.Controls.Add(this.PBx6);
+            this.Controls.Add(this.PBx5);
+            this.Controls.Add(this.PBx4);
+            this.Controls.Add(this.PBx3);
+            this.Controls.Add(this.PBx2);
+            this.Controls.Add(this.PBx1);
+            this.Controls.Add(this.RBtn4Joueurs);
+            this.Controls.Add(this.RBtn3Joueurs);
+            this.Controls.Add(this.RBtn2Joueurs);
             this.Name = "Form1";
             this.Text = "Snake & Lader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PBx9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBx8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBx7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBx6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBx5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBx4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBx3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBx2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PBx1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ComboBox CmBxNbJoueur;
+        private System.Windows.Forms.RadioButton RBtn2Joueurs;
+        private System.Windows.Forms.RadioButton RBtn3Joueurs;
+        private System.Windows.Forms.RadioButton RBtn4Joueurs;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.PictureBox PBx1;
+        private System.Windows.Forms.PictureBox PBx2;
+        private System.Windows.Forms.PictureBox PBx3;
+        private System.Windows.Forms.PictureBox PBx4;
+        private System.Windows.Forms.PictureBox PBx5;
+        private System.Windows.Forms.PictureBox PBx6;
+        private System.Windows.Forms.PictureBox PBx7;
+        private System.Windows.Forms.PictureBox PBx8;
+        private System.Windows.Forms.PictureBox PBx9;
+        private System.Windows.Forms.Button BtnJoueur;
     }
 }
 
--- Snake et Laders Anime/Form1.cs
+++ Snake et Laders Anime/Form1.cs
@@ -16,5 +16,25 @@ namespace Snake_et_Laders_Anime
         {
             InitializeComponent();
         }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void BtnJoueur_Click(object sender, EventArgs e)
+        {
+            //GameBoard form2 = new GameBoard();
+            //form2.ShowDialog();
+            this.Hide();
+            var form2 = new GameBoard();
+            form2.Closed += (s, args) => this.Close();
+            form2.Show();
+        }
     }
 }
