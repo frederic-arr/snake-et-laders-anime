9ca406f8be1c8c835a1ea0bb4b20ada531bc73da
Frédéric Arroyo <71824723+frederic-arr@users.noreply.github.com>
2022-05-16 13:12:30 +0200

    feat(game board): add cells

--- Snake et Laders Anime/GameBoard.cs
+++ Snake et Laders Anime/GameBoard.cs
@@ -12,8 +12,27 @@ namespace Snake_et_Laders_Anime
 {
     public partial class GameBoard : Form
     {
+        const int COLUMN_COUNT = 6;
+        const int LINE_COUNT = 5;
+        const int CELL_SIZE = 128;
         public GameBoard()
         {
+
+            for (int col = 0; col < COLUMN_COUNT; col++)
+            {
+                for (int ln = 0; ln < LINE_COUNT; ln++)
+                {
+                    Label label = new Label();
+
+                    label.Location = new Point(ln * CELL_SIZE, 512 - (col * CELL_SIZE));
+                    label.Name = "label1";
+                    label.TabIndex = 1;
+                    label.Size = new Size(128, 128);
+                    label.Text = $"({ln}, {col})";
+                    label.TextAlign = ContentAlignment.MiddleCenter;
+                    Controls.Add(label);
+                }
+            }
             InitializeComponent();
         }
     }
